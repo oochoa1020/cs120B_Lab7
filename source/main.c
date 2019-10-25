f5dd7cc69634af9f86f913a26b1b2e20e6a2f018
oochoa1020 <oochoa1020@gmail.com>
2019-10-24 17:45:19 -0700

    example

--- source/main.c
+++ source/main.c
@@ -8,16 +8,20 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include "io.h"
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
+    DDRC = 0x00; PORTC = 0xFF;
+    DDRD = 0xFF; PORTD = 0x00;
 
-    /* Insert your solution below */
+    LCD_init();
+
+    LCD_DisplayString(1, "Hello World");
     while (1) {
-
+	    continue;
     }
     return 1;
 }
